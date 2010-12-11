44c20db839838126547438287e4089e2915bc638
Joe Blaylock <jrbl@slac.stanford.edu>
2010-12-10 17:43:03 -0800

    extract_urls.py bug fixes; now it works

--- extract_urls.py
+++ extract_urls.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Extract http urls from a file.  Assumes roughly one url per line.
+#
+# Original version was a 1-liner:
+# [open("urls-http-"+S, 'w').writelines([m.group('url')+'\n' for m in [re.search('(?P<url>http:\/\/[a-zA-Z0-9_-]+\.(?:[a-zA-Z0-9_-]+\.?)+(?:[a-zA-Z0-9_./-]+)*)', l) for l in open("descrip-http-"+S)] if m != None]) for S in [x[13:] for x in os.listdir(os.getcwd()) if x.startswith('descrip-http-')]]
 
 import os
 import re
 import sys
 
-def get_http_files():
+def get_http_files(prefix):
     for filename in os.listdir(prefix):
         if filename.endswith('-http.spi'):
             yield filename
@@ -22,11 +25,14 @@ def get_file_urls(filename, urlmatch):
 def main(prefix):
     urlmatch = re.compile('(?P<url>http:\/\/[a-zA-Z0-9_-]+\.(?:[a-zA-Z0-9_-]+\.?)+(?:[a-zA-Z0-9_./-]+)*)')
     for filename in get_http_files(prefix):
-        year = filename[12:]
-        urlfile = open(prefix+'/'+year+"-urls.spi" + year, 'w')
-        for url in get_file_urls(prefix+'/'+filename, urlmatch):
+        year = filename[:4]
+        urlfile = open(prefix+year+"-urls.spi", 'w')
+        for url in get_file_urls(prefix+filename, urlmatch):
             urlfile.write(url + '\n')
         print "done writing", year+"-urls.spi"
 
 if __name__ == "__main__":
+    prefix = sys.argv[1]
+    if prefix[-1] != '/':
+        prefix += '/'
     main(sys.argv[1])
